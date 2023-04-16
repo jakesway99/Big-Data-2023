a50d0f00378d74fa8e0d863297e0042a0ff1899a
Rose Wang <55362828+princemeow0626@users.noreply.github.com>
2023-04-16 15:45:53 -0400

    Add files via upload
    April 16th progress

---
+++ complaint.py
@@ -0,0 +1,80 @@
+from pyspark import SparkContext, SparkConf
+from pyspark.sql import SparkSession
+from pyspark.sql import functions as F
+
+
+cf = SparkConf()
+cf.set("spark.submit.deployMode", "client")
+sc = SparkContext.getOrCreate(cf)
+
+spark = SparkSession \
+    .builder \
+    .appName("NYC Complaint Analysis") \
+    .config("spark.some.config.option", "some-value") \
+    .getOrCreate()
+
+complaint_historic_df = spark.read.csv(path='NYPD_Complaint_Data_Historic.csv',header=True)
+complaint_current_df = spark.read.csv(path='NYPD_Complaint_Data_Current__Year_To_Date_.csv',header=True)
+
+
+#print(complaint_historic_df.count()) # 7825499
+#print(complaint_current_df.count()) # 531768
+
+#complaint_historic_df.printSchema()
+#complaint_current_df.printSchema()
+
+complaint_historic_df.createOrReplaceTempView("complaint_historic")
+complaint_current_df.createOrReplaceTempView("complaint_current")
+
+complaint_df = spark.sql("SELECT CMPLNT_NUM, CMPLNT_FR_DT, CMPLNT_TO_DT, ADDR_PCT_CD, "
+                         "KY_CD, OFNS_DESC, BORO_NM, SUSP_AGE_GROUP, SUSP_RACE, SUSP_SEX, "
+                         "VIC_AGE_GROUP, VIC_RACE, VIC_SEX, Latitude, Longitude "
+                         "FROM complaint_historic "
+                         "UNION ALL "
+                         "SELECT CMPLNT_NUM, CMPLNT_FR_DT, CMPLNT_TO_DT, ADDR_PCT_CD, "
+                         "KY_CD, OFNS_DESC, BORO_NM, SUSP_AGE_GROUP, SUSP_RACE, SUSP_SEX, "
+                         "VIC_AGE_GROUP, VIC_RACE, VIC_SEX, Latitude, Longitude "
+                         "FROM complaint_current")
+
+# the unioned dataset has 8357267 rows
+#print(complaint_df.count())
+
+# CMPLNT_FR_DT is Exact date of occurrence for the reported event (or starting date of occurrence, if CMPLNT_TO_DT exists)
+# CMPLNT_TO_DT is Ending date of occurrence for the reported event, if exact time of occurrence is unknown
+# create a Crime_Date column(pick CMPLNT_FR_DT if it exists, otherwise pick CMPLNT_TO_DT)
+complaint_df2 = complaint_df.withColumn('Crime_Date', F.when(complaint_df['CMPLNT_FR_DT']=='null', 'CMPLNT_TO_DT').otherwise(complaint_df['CMPLNT_FR_DT']))
+complaint_df2.show()
+
+# interested in complaints happened after 2015-01-01, excludes rows that have date before 2015-01-01
+complaint_after_2015_df = complaint_df2.withColumn('Crime_Date', F.to_date('Crime_Date', 'M/d/y')).filter((F.col('Crime_Date') >= F.lit('2015-01-01')))
+
+complaint_after_2015_df.createOrReplaceTempView("complaint_after_2015")
+
+# how many complaints occurred each month after 2015-01-01?
+complaint_after_2015_df.groupBy(F.year('Crime_Date'), F.month('Crime_Date')).count().orderBy(F.year('Crime_Date'), F.month('Crime_Date')).show()
+
+# want to analyze complaints based on offense types
+# examine the null values in offense description column, and their correlated key codes
+spark.sql("SELECT DISTINCT(KY_CD, OFNS_DESC) FROM complaint_after_2015 WHERE OFNS_DESC is null ").show()
+
+# want to exclude the rows that have null in OFNS_DESC column
+complaint_after_2015_df2 = complaint_after_2015_df.filter(complaint_after_2015_df['OFNS_DESC'].isNotNull())
+
+# how many complaints based on offense types occurred each month after 2015-01-01?
+complaint_after_2015_df2.groupBy(F.year('Crime_Date'), F.month('Crime_Date'),'OFNS_DESC').count().orderBy(F.year('Crime_Date'), F.month('Crime_Date'), 'OFNS_DESC').show()
+
+# want to analyze victim race
+# examine the values in victim race column
+complaint_after_2015_df.select("VIC_RACE").distinct().show()
+
+# there are null and UNKNOWN values in the victim race column
+# want to change the null values and '(null)' values to UNKNOWN in victim race column
+complaint_after_2015_df3 = complaint_after_2015_df.withColumn('VIC_RACE', F.when(complaint_after_2015_df['VIC_RACE'].isNull(), 'UNKNOWN').otherwise(complaint_after_2015_df['VIC_RACE']))
+complaint_after_2015_df3 = complaint_after_2015_df3.withColumn('VIC_RACE', F.when(complaint_after_2015_df3['VIC_RACE']=='(null)', 'UNKNOWN').otherwise(complaint_after_2015_df3['VIC_RACE']))
+complaint_after_2015_df3.select("VIC_RACE").distinct().show()
+
+# victim races VS. complaint count each month after 2015-01-01
+complaint_after_2015_df3.groupBy(F.year('Crime_Date'), F.month('Crime_Date'), 'VIC_RACE').count().orderBy(F.year('Crime_Date'), F.month('Crime_Date'),  'VIC_RACE').show()
+
+
+
